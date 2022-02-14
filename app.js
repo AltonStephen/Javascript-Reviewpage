7e3b23e970c047f10786d518adefa5480992fc38
AltonStephen <52788898+AltonStephen@users.noreply.github.com>
2022-02-14 13:22:40 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,93 @@
+// local reviews data
+const reviews = [
+    {
+      id: 1,
+      name: "susan smith",
+      job: "web developer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+      text:
+        "We love what we do and we do what our clients love & work with great clients all over the world to create thoughtful and purposeful websites",
+    },
+    {
+      id: 2,
+      name: "anna johnson",
+      job: "web designer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+      text:
+        "Remember, the web isn't about control. If a visitor to your site is familiar with using a browser's native form doodad, you won't be doing them any favors if you override the browser functionality with your own widget, even if you think your widget looks better.",
+    },
+    {
+      id: 3,
+      name: "peter jones",
+      job: "intern",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+      text:
+        "Your website is the center of your digital eco-system, like a brick and mortar location, the experience matters once a customer enters, just as much as the perception they have of you before they walk through the door.",
+    },
+    {
+      id: 4,
+      name: "bill anderson",
+      job: "the boss",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+      text:
+        "They say that war is death's best friend, but I must offer you a different point of view on that one. To me, war is like the new boss who expects the impossible. He stands over your shoulder repeating one thin, incessantly: 'Get it done, get it done.' So you work harder. You get the job done. The boss, however, does not thank you. He asks for more. ",
+    },
+  ];
+  // select items
+  const img = document.getElementById("person-img");
+  const author = document.getElementById("author");
+  const job = document.getElementById("job");
+  const info = document.getElementById("info");
+  
+  const prevBtn = document.querySelector(".prev-btn");
+  const nextBtn = document.querySelector(".next-btn");
+  const randomBtn = document.querySelector(".random-btn");
+  
+  // set starting item
+  let currentItem = 0;
+  
+  // load initial item
+  window.addEventListener("DOMContentLoaded", function () {
+    const item = reviews[currentItem];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+  });
+  
+  // show person based on item
+  function showPerson(person) {
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+  }
+  // show next person
+  nextBtn.addEventListener("click", function () {
+    currentItem++;
+    if (currentItem > reviews.length - 1) {
+      currentItem = 0;
+    }
+    showPerson(currentItem);
+  });
+  // show prev person
+  prevBtn.addEventListener("click", function () {
+    currentItem--;
+    if (currentItem < 0) {
+      currentItem = reviews.length - 1;
+    }
+    showPerson(currentItem);
+  });
+  // show random person
+  randomBtn.addEventListener("click", function () {
+    console.log("hello");
+  
+    currentItem = Math.floor(Math.random() * reviews.length);
+    showPerson(currentItem);
+  });
+  
